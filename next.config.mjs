48f5d5ba143c191ee895a79775b42c589c7447d7
kona_srinivas <kona_srinivas@apple.com>
2026-06-26 21:47:27 +0530

    Update next.config.mjs with dynamic basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,20 @@ const withPWA = withPWAInit({
   skipWaiting: true,
 });
 
+// Detect if we are building in GitHub Actions
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+// Change this to your repository name if hosting on srinivaskona7.github.io/<repo-name>
+// Set to empty string if using a Custom Domain on GitHub Pages
+const repoName = "latha-srinivas"; 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   reactStrictMode: true,
   trailingSlash: true,
+  basePath: isGithubActions ? `/${repoName}` : "",
 };
 
 export default withPWA(nextConfig);
+
